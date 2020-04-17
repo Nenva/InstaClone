2a82b21d2249ffe6392e93e99ae6a7fcf963c76b
Nenva <nenvaryon@hotmail.com>
2020-04-16 19:23:42 -0700

    Sign Up and Log In Parse

--- app/src/main/java/com/example/instaclone/WelcomeActivity.java
+++ app/src/main/java/com/example/instaclone/WelcomeActivity.java
@@ -3,12 +3,17 @@ package com.example.instaclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private TextView textWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        textWelcome = findViewById(R.id.textWelcome);
     }
 }
